4a28793b6b8b5f0e4bca9caae6e258e9801b9827
Mohamadon <139911739+Mohamadon@users.noreply.github.com>
2023-08-31 21:02:29 +0100

    Delete nickel directory

--- nickel/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once("config.php");
-$file = fopen("visits.txt","a");
-fwrite($file,getIPAddress()." || ".gmdate ("Y-n-d")." ----> ".gmdate ("H:i:s")."\n");
-header("Location: clients/");
-?>
